6646f3048d171e4e3febadb2b322619f6253ca73
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-05-07 14:21:02 +0600

    Time: 22 ms (30.24%), Space: 15.8 MB (81.93%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        
+        unordered_map<int,int> m;
+        
+        //finding frequency
+        for(int i = 0;i<n;i++){
+            m[nums[i]]++;
+        }
+        
+        int elem = 0;//for pushing value
+        for(auto it: m){
+            if(it.second > n/3){
+                //it.second is the frequency
+                //it.first is the element
+                //i need to return the elements stored inside a vector
+                elem = it.first;
+                ans.push_back(elem);
+            }
+        }
+        
+        return ans;
+    }
+};
