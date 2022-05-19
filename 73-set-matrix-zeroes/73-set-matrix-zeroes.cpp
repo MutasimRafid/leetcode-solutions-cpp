dce7ad7e08d3a0973dcd73afa8e4ac3fba0dc076
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-05-19 13:58:03 +0600

    Time: 16 ms (73.94%), Space: 13.3 MB (54.16%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,7 +1,68 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       //--------------------------------------------
+       //Approach 3:
+        //Most optimized solution: in place(no extra space)
+        //imagine col = an array that stores, which col has zero 
+        //algo: there was 1 array col to store which col contains zero
+        //now will use the 1st row to store the same information that was in col array
+        //won't use row array, instead will use bool variable in each iteration to check if that particular row has any zero or not
+        //but there is 1 problem, we need to know if 1st row has any zero or not 
+        //if 1st row doesn't have any zero, then (except that column has zero) all other element will remain same
+        //will use a variable to keep track if 1st row has any zero or not
+        int H = matrix.size();
+        int W = matrix[0].size();
+        
+       //checking if 1st row has any zero or not
+        bool first_row_zero = false;
+        for(int col = 0;col<W;col++){
+            if(matrix[0][col] == 0){
+                first_row_zero = true;
+            }
+        }
+        
+        //storing col array information in 1st row
+        for(int row = 0;row<H;row++){
+            for(int col = 0;col<W;col++){
+                if(matrix[row][col] == 0){
+                    matrix[row][col] == 0;
+                    //1st row
+                    matrix[0][col] = 0;
+                }
+            }
+        }
+        
+        //starting from 2nd row: i = 1.... not i = 0 
+        for(int row = 1;row<H;row++){
+            //if that particular row has any zero or not(loop started from 2nd row)
+            bool contains_zero = false; //initiating as false
+            for(int col = 0;col<W;col++){
+                if(matrix[row][col] == 0){
+                    contains_zero = true;//that particular row has zero
+                    break;
+                }
+            }
+            
+            //now if any was zero is found, make that col element zero
+            for(int col = 0;col<W;col++){
+                //if that row has zero or col has zero
+                if(contains_zero || matrix[0][col] == 0){
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+        
+        //if first row has any zero, make 1st row zero
+        if(first_row_zero){
+            for(int col = 0;col<W;col++){
+                matrix[0][col] = 0;
+            }
+        }
+        
+        
+        
+        /*
+        //---------------------------------------------------------------------------------------------------------------
         //approach1: Brute force
         //Algo: take 2 extra array, 1 for ROW and 1 for col
         //store which row is zero in row array and store which col is zero in col array
@@ -30,53 +91,12 @@ public:
                 }
             }
         }
-        
-        
-        
-        //---------------------------------------------
-        /*
-        //read khata for explanation
-        // n = row size, m = col size
-        int n = matrix.size(),m = matrix[0].size();
-        vector<int> row(n,1);
-        vector<int> col(m,1);
-        
-        //row vector element 0, if that particular row in main matrix is 0
-        //col vector element 0, if that particular col in main matrix is 0
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(matrix[i][j]==0){
-                    row[i] = 0;
-                    col[j] = 0;
-                }
-            }
-        }
-        //making all row zero in the main matrix
-        for(int i = 0;i<n;i++){
-            if(row[i]==0){
-                //till now reached row number
-                //if there is a zero in that row
-                //now need to make all elements 0 of that particular row
-                //that means we need to go through col number
-                for(int j = 0;j<m;j++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        
-        //making all col zero in the main matrix
-        for(int j = 0;j<m;j++){
-            if(col[j]==0){
-                //till now reached col number
-                //if there is a zero in that col
-                //now need to make all elements 0 of that particular col
-                //that means we need to go through row number
-                for(int i = 0;i<n;i++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
         */
+        
+        
+        
+       
+       
         //---------------------------------------------------------------
         
     }
