5b60c2eba8be39fd7ffd4052bb80910abb41ac14
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-05-20 16:41:45 +0600

    Time: 3 ms (33.16%), Space: 6.5 MB (33.58%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,18 +4,32 @@ public:
         vector<vector<int>> ans;
         
         for(int i = 0;i<numRows;i++){
-            //when i = 0, element: 1 1
-            //when i = 1, element: 1 2 1
-            //when i = 2, element: 1 3 3 1
-            //so, vector size i+1
+            //i=0   numRows = 1
+            //i = 1 numRows = 1 1
+            //i = 2 numRows = 1 2 1
+            
+            
+            //i = 0, vector size i+1 = 0+1 = 1
+            //filling all element with 1
+            //1st and last element is always 1
+            //so filling all value with 1
+            //will change the middle value later
             vector<int> v(i+1,1);
             
-            for(int j = 1;j<i;j++){
+            //middle values change
+            //let's say: test case: i = 2
+            //i=0   numRows = 1
+            //i = 1 numRows = 1 1
+            //i = 2 numRows = 1 2 1
+            //1st need to go previous row (i-1)
+            //then current col = j + previous col = j-1
+            //loop starting from j = 1 = 2nd col to before last col j<= i-1 
+            for(int j = 1;j<=i-1;j++){
                 //current row i, previous row i-1
                 //current col j, previous col j-1
                 v[j] = ans[i-1][j] + ans[i-1][j-1];
             }
-            
+            //lastly value needs to be pushed
             ans.push_back(v);
         }
         
