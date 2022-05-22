252c95d2f5a9afab572b1dfd837df8f23208956b
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-05-22 13:51:59 +0600

    Time: 190 ms (33.03%), Space: 93.5 MB (13.07%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        //think about profit from normal order
+        //TC: O(n) SC: O(1)
         int maxPro = 0; //maximum profit
         int minPrice = INT_MAX; //minimum price
         int currPro = 0; //current profit
