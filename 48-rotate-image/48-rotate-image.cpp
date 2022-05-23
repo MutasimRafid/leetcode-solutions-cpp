6fb7166c70eff9b2ad9ebac0e5b5bbf8c7484f4e
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-05-23 13:05:14 +0600

    Time: 3 ms (59.07%), Space: 7.2 MB (34.23%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        /*
+        //Brute force solution:
+        //approach: Take an extra array. then matrix 1st row = new arr last col.... so on...
+        //TC: O(n^2) SC: O(n^2)
+                    vector < vector < int >> rotate(vector < vector < int >> & matrix) {
+        int n = matrix.size();
+        vector < vector < int >> rotated(n, vector < int > (n, 0));
+         for (int i = 0; i < n; i++) {
+           for (int j = 0; j < n; j++) {
+               rotated[j][n - i - 1] = matrix[i][j];
+            }
+         }
+        return rotated;
+        }
+        
+        */
+        
+        //-----------------------------------------------------------------------------
+        //optimized code:TC: O(n^2)+ O(n^2)... SC: O(1)
+        //by observation we can see that after transpose if we reverse each row then that's the ans
+        //1. transpose the matrix
+        //2. reverse each row
+        
         int n = matrix.size(); //row and col size is same as it is a square matrix
         
         //now transpose the matrix
