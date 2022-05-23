a8367cd0adcfe5bd6526111b2b08d725e2e074d3
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-05-23 21:12:44 +0600

    Time: 7 ms (44.08%), Space: 9.5 MB (53.55%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,11 +1,48 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        /*
+        Approach 1: Brute force
+        TC: O(n*m)  sc: O(1)
+        algo: run nested loop. Traverse each cell/element. Find the target element
+        
+        //code:
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int n = row;
+        int m = col;
+        
+        for(int i = 0;i<n;i++){
+         for(int j = 0;j<m;j++){
+           if(matrix[i][j] == target){
+              return true;
+           }
+         }
+        }
+        return false;
+        
+        */
+        //---------------------------------------------------------------
+        //Approach 2: using binary search 
+        //TC: O(n*m) SC: O(1)
+        //As each row is sorted. We can use binary search. 
+        //as whole matrix is sorted like: each last element of a row < 1st element of next row
+        //so we can think it as a 1D Matrix. And we will use binary search
+        //But we won't use any extra space. we will use the corresponding matrix 
+        //will use imaginary matrix
+        
+        
+        //co ordinates of 1D array from 2D array
+        //linear row = mid/col... 
+        //linear col = mid%col
+        
+        
         int row = matrix.size();
         int col = matrix[0].size();
         
-        int start = 0;
-        int end = row * col - 1;
+        int start = 0; //linear/1D start
+        int end = row * col - 1; //linear/1D end
         
         int mid = start + (end - start)/2;
         
@@ -25,9 +62,11 @@ public:
             }
             
             
-            
+            //reset mid
             mid = start + (end - start)/2;
         }
         return 0;
     }
+    //Approach 3: 2 pointer is not written yet
+    //Most optimized
 };
