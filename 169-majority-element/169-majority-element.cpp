1a7f90113b083f89416a8b9e9887257882921c58
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-05-24 13:44:29 +0600

    Time: 35 ms (20.28%), Space: 19.6 MB (59.94%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,10 +1,35 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        /*
+        //Approach 1: Brute Force
+        //use nested loop
+        //TC: O(n^2)... SC:O(1)
+        int cnt = 0;
+        int ans;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+         for(int j = 0;j<n;j++){
+           if(nums[i] == nums[j]){
+               cnt++;
+               if(cnt>n/2){
+                 ans = nums[i];
+                 return ans;
+                 break;
+               }
+           }
+         }
+        }
+        
+        */
+        //----------------------------------------------------------
+        
+        //Approach 2: optimized
         //using frequency map
         //tc: o(nlogn) ... because of map
         //sc: o(n)
         
+        //std::map is a sorted associative container that contains key-value pairs with unique keys. Keys are sorted             by using the comparison function
         unordered_map<int,int> m;
         int n = nums.size();
         
