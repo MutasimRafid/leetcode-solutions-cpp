0f332f39624c4aaaab4b10509fcd1dd9bb3c5703
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-06-19 14:41:22 +0600

    Added solution - LeetHub

---
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  //Your code here
+  string st = "";
+  int s = str.size() - 1;
+  for(int i = s;i>=0;i--){
+      st = st + str[i];
+  }
+  return st;
+}
